8c33d9843c01c84df384fd8491c741b85a7d6f7f
Liza_silent <elizavetad@ya.ru>
2022-05-23 17:35:42 +0300

    html started

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const button = document.querySelector(".container__button");
+
+
+
+
+function checkButton() {
+
+ button.addEventListener("click", console.log("всё работает"));
+    
+}
+
+// checkButton();
+
+
