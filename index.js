d9b034d9afbfa1f930ef04f561377ab06cb92d71
Liza_silent <elizavetad@ya.ru>
2022-05-24 18:59:08 +0300

    button fixed

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 
 const button = document.querySelector(".container__button");
 
+const workers = [
+    {
+        name: "Сеня",
+        avatar: "/images/3d0bf497c0f9487aa3a74918c87b2cda.png",
+        bio: "Потрясающий и блистательный"
+        
+      },
+
+      {
+        name: "Тест",
+        avatar: "",
+        bio: ""
+        
+      },
+
+      {
+        name: "",
+        avatar: "",
+        bio: ""
+        
+      },
+
+      {
+        name: "",
+        avatar: "",
+        bio: ""
+        
+      },
+]
 
 
 
 function checkButton() {
 
- button.addEventListener("click", console.log("всё работает"));
+    console.log("всё работает");
+
     
 }
 
-// checkButton();
+// for (let i = 0; i < groceries.length; i++) {
+//   let item = groceries[i];
+//   }
 
-
+button.addEventListener("click", checkButton);
