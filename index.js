3ac68a4368af27f73525d0b214c1dbda797f798f
Liza_silent <elizavetad@ya.ru>
2022-05-25 16:41:33 +0300

    random function done

--- index.js
+++ index.js
@@ -1,47 +1,50 @@
-
 const button = document.querySelector(".container__button");
 
 const workers = [
-    {
-        name: "Сеня",
-        avatar: "/images/3d0bf497c0f9487aa3a74918c87b2cda.png",
-        bio: "Потрясающий и блистательный"
-        
-      },
+  {
+    name: "Сеня",
+    avatar: "/images/3d0bf497c0f9487aa3a74918c87b2cda.png",
+    bio: "Потрясающий и блистательный",
+  },
 
-      {
-        name: "Тест",
-        avatar: "",
-        bio: ""
-        
-      },
+  {
+    name: "Тест",
+    avatar: "",
+    bio: "",
+  },
 
-      {
-        name: "",
-        avatar: "",
-        bio: ""
-        
-      },
+  {
+    name: "лала",
+    avatar: "лалала",
+    bio: "лалалалала",
+  },
 
-      {
-        name: "",
-        avatar: "",
-        bio: ""
-        
-      },
-]
+  {
+    name: "лоло",
+    avatar: "лололо",
+    bio: "лололололо",
+  },
+];
 
 
 
 function checkButton() {
 
-    console.log("всё работает");
+  let random = Math.floor(Math.random() * workers.length);
 
-    
+  console.log(workers[random]);
 }
+
+button.addEventListener("click", checkButton);
+
+// function getRandomNumber(low, high) {
+//   let r = Math.floor(Math.random() * (high - low + 1)) + low;
+//   return r;
+// }
 
 // for (let i = 0; i < groceries.length; i++) {
 //   let item = groceries[i];
 //   }
 
-button.addEventListener("click", checkButton);
+
+
